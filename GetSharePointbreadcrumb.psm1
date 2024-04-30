ed33fec268d8466391af8956924521ceed5a14f3
VWNET\mura <mura@vwnet.jp>
2024-04-30 11:31:15 +0900

    オンライン更新を追加

--- GetSharePointbreadcrumb.psm1
+++ GetSharePointbreadcrumb.psm1
@@ -1,4 +1,51 @@
-﻿function GetSharePointbreadcrumb([switch]$Debug){
+﻿function GetSharePointbreadcrumb([switch]$Debug, [switch]$VertionCheck){
+
+	if( $VertionCheck ){
+		$ScriptName = "GetSharePointbreadcrumb"
+		$GitHubName = "MuraAtVwnet"
+		$Module = $ScriptName + ".psm1"
+		$Installer = "Install" + $ScriptName + ".ps1"
+		$UnInstaller = "UnInstall" + $ScriptName + ".ps1"
+		$Vertion = "Vertion" + $ScriptName + ".txt"
+		$VertionTemp = "VertionTemp" + $ScriptName + ".tmp"
+
+		$Update = $False
+
+		if( -not (Test-Path ~/$Vertion)){
+			$Update = $True
+		}
+		else{
+			$LocalVertion = Get-Content -Path ~/$Vertion
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$VertionTemp
+			$NowVertion = Get-Content -Path ~/$VertionTemp
+			Remove-Item ~/$VertionTemp
+
+			if( $LocalVertion -ne $NowVertion ){
+				$Update = $True
+			}
+		}
+
+		if( $Update ){
+			Write-Output "最新版に更新します"
+			Write-Output "更新完了後、PowerShell プロンプトを開きなおしてください"
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/$Module -OutFile ~/$Module
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/install.ps1 -OutFile ~/$Installer
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/uninstall.ps1 -OutFile ~/$UnInstaller
+			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$Vertion
+			& ~/$Installer
+			Remove-Item ~/$Module
+			Remove-Item ~/$Installer
+			Write-Output "更新完了"
+			Write-Output "PowerShell プロンプトを開きなおしてください"
+		}
+		else{
+			Write-Output "更新の必要はありません"
+		}
+
+		return
+	}
+
+
 	$URL = Get-Clipboard
 
 	$RejectString = "^.+AllItems\.aspx\?"
--- Readme.txt
+++ Readme.txt
@@ -25,9 +25,11 @@ $GitHubName = "MuraAtVwnet"
 $Module = $ScriptName + ".psm1"
 $Installer = "Install" + $ScriptName + ".ps1"
 $UnInstaller = "UnInstall" + $ScriptName + ".ps1"
+$Vertion = "Vertion" + $ScriptName + ".txt"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/$Module -OutFile ~/$Module
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/install.ps1 -OutFile ~/$Installer
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/uninstall.ps1 -OutFile ~/$UnInstaller
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$Vertion
 & ~/$Installer
 Remove-Item ~/$Module
 Remove-Item ~/$Installer
