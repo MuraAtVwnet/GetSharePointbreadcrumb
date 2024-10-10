64ec707f52bdd5354cefd19d37bd933395bd08c4
VWNET\mura <mura@vwnet.jp>
2024-10-10 13:01:27 +0900

    バージョンアップを関数化した

--- GetSharePointbreadcrumb.psm1
+++ GetSharePointbreadcrumb.psm1
@@ -1,50 +1,55 @@
-﻿function GetSharePointbreadcrumb([switch]$Debug, [switch]$VertionCheck){
+﻿function VertionCheck(){
+	$ScriptName = "GetSharePointbreadcrumb"
+	$GitHubName = "MuraAtVwnet"
+	$Module = $ScriptName + ".psm1"
+	$Installer = "Install" + $ScriptName + ".ps1"
+	$UnInstaller = "UnInstall" + $ScriptName + ".ps1"
+	$Vertion = "Vertion" + $ScriptName + ".txt"
+	$VertionTemp = "VertionTemp" + $ScriptName + ".tmp"
 
-	if( $VertionCheck ){
-		$ScriptName = "GetSharePointbreadcrumb"
-		$GitHubName = "MuraAtVwnet"
-		$Module = $ScriptName + ".psm1"
-		$Installer = "Install" + $ScriptName + ".ps1"
-		$UnInstaller = "UnInstall" + $ScriptName + ".ps1"
-		$Vertion = "Vertion" + $ScriptName + ".txt"
-		$VertionTemp = "VertionTemp" + $ScriptName + ".tmp"
+	$Update = $False
 
-		$Update = $False
+	if( -not (Test-Path ~/$Vertion)){
+		$Update = $True
+	}
+	else{
+		$LocalVertion = Get-Content -Path ~/$Vertion
+		Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$VertionTemp
+		$NowVertion = Get-Content -Path ~/$VertionTemp
+		Remove-Item ~/$VertionTemp
 
-		if( -not (Test-Path ~/$Vertion)){
+		if( $LocalVertion -ne $NowVertion ){
 			$Update = $True
 		}
-		else{
-			$LocalVertion = Get-Content -Path ~/$Vertion
-			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$VertionTemp
-			$NowVertion = Get-Content -Path ~/$VertionTemp
-			Remove-Item ~/$VertionTemp
-
-			if( $LocalVertion -ne $NowVertion ){
-				$Update = $True
-			}
-		}
-
-		if( $Update ){
-			Write-Output "最新版に更新します"
-			Write-Output "更新完了後、PowerShell プロンプトを開きなおしてください"
-			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/$Module -OutFile ~/$Module
-			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/install.ps1 -OutFile ~/$Installer
-			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/uninstall.ps1 -OutFile ~/$UnInstaller
-			Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$Vertion
-			& ~/$Installer
-			Remove-Item ~/$Module
-			Remove-Item ~/$Installer
-			Write-Output "更新完了"
-			Write-Output "PowerShell プロンプトを開きなおしてください"
-		}
-		else{
-			Write-Output "更新の必要はありません"
-		}
-
-		return
 	}
 
+	if( $Update ){
+		Write-Output "最新版に更新します"
+		Write-Output "更新完了後、PowerShell プロンプトを開きなおしてください"
+		Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/$Module -OutFile ~/$Module
+		Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/install.ps1 -OutFile ~/$Installer
+		Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/uninstall.ps1 -OutFile ~/$UnInstaller
+		Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ScriptName/master/Vertion.txt -OutFile ~/$Vertion
+		& ~/$Installer
+		Remove-Item ~/$Module
+		Remove-Item ~/$Installer
+		Write-Output "更新完了"
+		Write-Output "PowerShell プロンプトを開きなおしてください"
+	}
+	else{
+		Write-Output "更新の必要はありません"
+	}
+	return
+}
+
+
+function GetSharePointbreadcrumb([switch]$Debug, [switch]$VertionCheck){
+
+
+	if( $VertionCheck ){
+		VertionCheck
+		return
+	}
 
 	$URL = Get-Clipboard
 
@@ -75,7 +80,6 @@
 		$SharePointPath = $Matches.SPPath
 		$SharePointBreadcrumb = $SharePointPath -replace "\/", " > "
 		$ClipbordStrings += $SharePointBreadcrumb
-#		$ClipbordStrings += ""
 		$ClipbordStrings += $URL
 		$ClipbordStrings += ""
 		Set-Clipboard -Value $ClipbordStrings
